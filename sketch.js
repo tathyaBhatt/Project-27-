3fe3584b9bc9f26eee365788b73406a925c160ee
tathyaBhatt <75122109+tathyaBhatt@users.noreply.github.com>
2020-12-25 10:37:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,24 +6,18 @@ const Body = Matter.Body;
 const Constraint = Matter.Constraint ; 
 var bobObject1,bobObject2,bobObject3 ,bobObject4,bobObject5;
 var ground;
-var roofObject ; 
-
-function preload()
-{
-	
-}
-
+ 
 function setup() {
 	engine = Engine.create();
 	world = engine.world;
-	createCanvas(1300, 700);
+	createCanvas(800, 700);
 
 bobObject1=new Bob(300,350,50);
 bobObject2=new Bob(350,350,50);
 bobObject3=new Bob(400,350,50);
 bobObject4=new Bob(450,350,50);
 bobObject5=new Bob(500,350,50);
-ground = new Roof(400,200,300,20);
+ground = new Bar(400,10,300,20);
 sling1= new Rope(bobObject1.body,ground.body,-50*2,0);
 sling2= new Rope(bobObject2.body,ground.body,-25*2,0);
 sling3= new Rope(bobObject3.body,ground.body,-0*2,0);
@@ -66,6 +60,4 @@ function keyPressed() {
 	if (keyCode === UP_ARROW) 
 	{ Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-100,y:-100}); } 
 } 
-
-
 
